1b0547a803508110778a8e6d7bc771751da99eed
Song <quanpengsong@gmail.com>
2020-04-17 15:53:26 +0800

    :hammer: major refactoring is under way

--- app.js
+++ app.js
@@ -1,72 +1,34 @@
 ﻿'use strict';
-const debug = require('debug');
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+const http = require('http');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const fs = require('fs');
-
 const routes = require('./routes/index');
-const users = require('./routes/users');
-
+const compression = require('compression');
+const serveStatic = require('serve-static');
 const app = express();
-
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.get("/existingFile", function (req, res) {
-    var responceText = '';
-    const targetDir = process.cwd() + '/public/data';
-    console.log("Taregt dir: " + targetDir);
-    fs.readdir(targetDir, (err, files) => {
-        files.forEach(file => {
-            const temp = file + '\n';
-            responceText += temp;
-        });
-        res.send(responceText);
-    });
-});
-
-
-app.post('/getFile', function (req, res) {
-    const targetDir = process.cwd() + '/public/data/';
-    const targetFile = targetDir + req.body.filename;
-    fs.readFile(targetFile, 'utf-8', function (err, data) {
-        if (err) {
-        } else {
-            res.send(data);
-        }
+app.use(
+    serveStatic(path.join(__dirname, 'public'), {
+        maxAge: '600000'
     })
-});
-
-
+);
+app.use(compression());
 app.use('/', routes);
-app.use('/users', users);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
-
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
 
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
@@ -76,10 +38,5 @@ app.use(function (err, req, res, next) {
     });
 });
 
-
-app.set('port', process.env.PORT || 3001);
-
-
-const server = app.listen(app.get('port'), function () {
-    debug('Express server listening on port ' + server.address().port);
-});
+const server = http.createServer(app);
+server.listen(process.env.PORT || 3000);
--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,47 @@
 ﻿'use strict';
-var express = require('express');
-var fs = require('fs');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
 
-/* GET home page. */
 router.get('/', function (req, res) {
-    //res.render('index', { title: 'Express' });
-	console.log("Request homepage.");
-	res.sendFile(process.cwd()+'/editor.html');
+	res.render('index');
 });
 
 router.post('/', function (req, res) {
 	var code = req.body.code;
 	var filename = req.body.filename;
 	var folder = req.body.folder;
-	console.log("Code:\n" + code + "\nFile name: " + filename + "\nFolder: " + folder);
 	var dirPath = process.cwd() + '/public/data/';
 	var filePath = dirPath + filename;
-	console.log("Save data at: " + filePath);
 	fs.writeFile(filePath, code, function () {
-		res.sendFile(process.cwd() + '/editor.html');
+		res.sendFile(process.cwd() + '/index.ejs');
 		//res.send("Successfully storing data.");
 	});
-
 });
+
+// app.get("/existingFile", function (req, res) {
+// 	var responceText = '';
+// 	const targetDir = process.cwd() + '/public/data';
+// 	console.log("Taregt dir: " + targetDir);
+// 	fs.readdir(targetDir, (err, files) => {
+// 		files.forEach(file => {
+// 			const temp = file + '\n';
+// 			responceText += temp;
+// 		});
+// 		res.send(responceText);
+// 	});
+// });
+//
+//
+// app.post('/getFile', function (req, res) {
+// 	const targetDir = process.cwd() + '/public/data/';
+// 	const targetFile = targetDir + req.body.filename;
+// 	fs.readFile(targetFile, 'utf-8', function (err, data) {
+// 		if (err) {
+// 		} else {
+// 			res.send(data);
+// 		}
+// 	})
+// });
 
 module.exports = router;
